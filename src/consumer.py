063846e1749e07b1e6e4bd671a2ddc1d14f5d972
Julia Adriely <juliaadrisabino@gmail.com>
2026-09-17 08:09:46 -0300

    feat: implementa consumidor de pagamentos

---
+++ src/consumer.py
@@ -0,0 +1,36 @@
+import pika
+import json
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host="localhost",
+        port=5672,
+        credentials=pika.PlainCredentials("admin", "admin")
+    )
+)
+
+channel = connection.channel()
+
+channel.queue_declare(queue="pagamentos", durable=True)
+
+
+def receber_mensagem(ch, method, properties, body):
+    mensagem = json.loads(body)
+
+    print("Pagamento recebido:")
+    print(f"ID: {mensagem['pagamento_id']}")
+    print(f"Status: {mensagem['status']}")
+    print(f"Valor: R$ {mensagem['valor']:.2f}")
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_consume(
+    queue="pagamentos",
+    on_message_callback=receber_mensagem,
+    auto_ack=False
+)
+
+print("Aguardando pagamentos...")
+
+channel.start_consuming()
